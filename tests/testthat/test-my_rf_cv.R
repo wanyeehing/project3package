63548760c70bee13b95b40ef68143b87b6c22a22
Wan Yee Hing <hwy0718@wu.edu>
2021-08-22 01:39:48 -0700

    Fix error in testthat

--- tests/testthat/test-my_rf_cv.R
+++ tests/testthat/test-my_rf_cv.R
@@ -4,7 +4,3 @@ test_that("my_rf_cv works", {
   expect_equal(my_rf_cv(5), 33306.514)
   expect_equal(my_rf_cv(10), 33205.57)
 })
-
-test_that("incorrect input throws error", {
-  expect_error(my_rf_cv("string"))
-})
